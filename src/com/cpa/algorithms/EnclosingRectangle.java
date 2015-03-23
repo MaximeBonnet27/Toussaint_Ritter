d6743d2979b70dc82da24f60e091d8fe168d45dd
MaximeBonnet27 <maximebonnet27@gmail.com>
2015-03-23 17:21:19 +0100

    Commentaire Toussaint

--- src/com/cpa/algorithms/EnclosingRectangle.java
+++ src/com/cpa/algorithms/EnclosingRectangle.java
@@ -8,12 +8,30 @@ import com.cpa.geometry.Line;
 import com.cpa.geometry.Rectangle;
 import com.cpa.geometry.Vector;
 
+/**
+ * Classe comprenant les opérations nécessaires pour trouver le rectangle
+ * minimum. source : calipers.pdf
+ * 
+ * @author Maxime Bonnet
+ * 
+ */
 public class EnclosingRectangle {
-
+	/**
+	 * L'algorithme de Toussaint.
+	 * 
+	 * @param hull
+	 *            L'enveloppe convexe du nuage de points considéré
+	 * @return Le rectangle d'aire minimum comprennant tous les points du nuage.
+	 */
 	public static Rectangle computeToussaint(ArrayList<Point.Double> hull) {
-		// 1: Find the 4 extreme points.
 
+		// Première étape : On cherche les quatre points extremes (abscisses
+		// minimum & maximum, ordonnées minimum & maximum)
+
+		// On garde en mémoire les indexs des points i, j, k et l.
 		int index_i = 0, index_j = 0, index_k = 0, index_l = 0;
+		// Première position des points, utilisée pour vérifier quand on a fait
+		// un tour complet.
 		int index_i0, index_j0, index_k0, index_l0;
 		for (int i = 1; i < hull.size(); i++) {
 			if (hull.get(i).x < hull.get(index_i).x) {
@@ -28,58 +46,59 @@ public class EnclosingRectangle {
 			if (hull.get(i).y > hull.get(index_j).y) {
 				index_j = i;
 			}
-
 		}
 
 		index_i0 = index_i;
 		index_j0 = index_j;
 		index_k0 = index_k;
 		index_l0 = index_l;
-		// 2 : Create the lines of support.
+
+		// Deuxième étape : On initialise les lignes de support (lignes passant
+		// par les 4 points i, j, k et l)
 		Line support_i, support_j, support_k, support_l;
 		support_i = new Line(hull.get(index_i), new Vector(0, 1));
-		support_j = new Line(hull.get(index_j), new Vector(1,0));
-		support_k = new Line(hull.get(index_k), new Vector(0,-1));
-		support_l = new Line(hull.get(index_l), new Vector(-1,0));
+		support_j = new Line(hull.get(index_j), new Vector(1, 0));
+		support_k = new Line(hull.get(index_k), new Vector(0, -1));
+		support_l = new Line(hull.get(index_l), new Vector(-1, 0));
 
-		// 3 : Get the angles.
-
+		// Condition d'arrêt
 		boolean hullScanned = false;
+		// Auxiliaire à la condition d'arrêt, vérifie si le point concerné à
+		// bien avancé.
 		boolean iStepped = false, jStepped = false, kStepped = false, lStepped = false;
+		// Le nombre de fois que l'un des points i, j, k ou l passe par sa
+		// position initiale
 		int count = 0;
 
+		// Les cosinus entre les lignes de support et le prochain coté de
+		// l'enveloppe convexe
 		double cos_theta_i, cos_theta_j, cos_theta_k, cos_theta_l;
+		// La valeur maximum de ces cosinus (correspond à l'angle minimum)
 		double cos_theta_max;
 		int index_cos_max;
+		// Le rectangle resultat
 		Rectangle res = new Rectangle(support_i, support_j, support_k,
 				support_l);
+		// L'aire du rectangle resultat
 		double areaMin = Double.MAX_VALUE;
+		// Le rectangle actuel
 		Rectangle rect = res;
+		// Tant que l'on n'a pas fini de parcourir l'enveloppe convexe.
 		while (!hullScanned) {
-			/*System.out.println("I : " + index_i);
-			System.out.println("J : " + index_j);
-			System.out.println("K : " + index_k);
-			System.out.println("L : " + index_l);
-			 */
 			/*
-			if(index_i == index_j || index_i == index_k || index_i == index_l){
-				System.out.println("IIINN YOUR FACE");
-				System.out.println(index_i);
-			}
-			if(index_j == index_k || index_j == index_l){
-				System.out.println("JJJN YOUR FACE");
-				System.out.println(index_j);
-			}
-			if(index_k == index_l){
-				System.out.println("KKKN YOUR FACE");
-				System.out.println(index_k);
-			}
+			 * Pour chaque point, on calcule le cosinus entre sa ligne de
+			 * support et le prochain coté dans l'enveloppe convexe. De plus, on
+			 * garde en mémoire la valeur du cosinus maximum, et l'index du
+			 * point corresepondant.
 			 */
-			
-			cos_theta_i = GeometryTools.cos(support_i, new Line(hull.get(index_i),
-					hull.get((index_i + 1) % hull.size())));
-			cos_theta_j = GeometryTools.cos(support_j, new Line(hull.get(index_j),
-					hull.get((index_j + 1) % hull.size())));
+			cos_theta_i = GeometryTools.cos(
+					support_i,
+					new Line(hull.get(index_i), hull.get((index_i + 1)
+							% hull.size())));
+			cos_theta_j = GeometryTools.cos(
+					support_j,
+					new Line(hull.get(index_j), hull.get((index_j + 1)
+							% hull.size())));
 			if (cos_theta_i > cos_theta_j) {
 				cos_theta_max = cos_theta_i;
 				index_cos_max = index_i;
@@ -87,146 +106,111 @@ public class EnclosingRectangle {
 				cos_theta_max = cos_theta_j;
 				index_cos_max = index_j;
 			}
-			cos_theta_k = GeometryTools.cos(support_k, new Line(hull.get(index_k),
-					hull.get((index_k + 1) % hull.size())));
+			cos_theta_k = GeometryTools.cos(
+					support_k,
+					new Line(hull.get(index_k), hull.get((index_k + 1)
+							% hull.size())));
 			if (cos_theta_k > cos_theta_max) {
 				cos_theta_max = cos_theta_k;
 				index_cos_max = index_k;
 			}
-			cos_theta_l = GeometryTools.cos(support_l, new Line(hull.get(index_l),
-					hull.get((index_l + 1) % hull.size())));
+			cos_theta_l = GeometryTools.cos(
+					support_l,
+					new Line(hull.get(index_l), hull.get((index_l + 1)
+							% hull.size())));
 			if (cos_theta_l > cos_theta_max) {
 				cos_theta_max = cos_theta_l;
 				index_cos_max = index_l;
 			}
-			// 4 : The min point steps on.
+
+			/*
+			 * Quatrième étape : On fait avancer le point correspondant à
+			 * l'angle minimum. Sa ligne support est alors confondue avec le
+			 * prochain coté de l'enveloppe convexe, et on détermine les trois
+			 * autres lignes de support en fonction de celle-ci (orthogonale /
+			 * inverse)
+			 */
+
 			if (index_cos_max == index_i) {
 				support_i = new Line(hull.get(index_i), hull.get((index_i + 1)
 						% hull.size()));
-				support_j = new Line(hull.get(index_j),
-						support_i.director.normal().invert());
+				support_j = new Line(hull.get(index_j), support_i.director
+						.normal().invert());
 				support_k = new Line(hull.get(index_k),
 						support_i.director.invert());
-				support_l = new Line(hull.get(index_l), support_j.director
-						.invert());
+				support_l = new Line(hull.get(index_l),
+						support_j.director.invert());
 
 				index_i = (index_i + 1) % hull.size();
 				support_i.point = hull.get(index_i);
-
-				/*if(index_i == index_j){
-          index_j = (index_j + 1) % hull.size();
-          support_j.point = hull.get(index_j);
-        }else if(index_i == index_k){
-          index_k = (index_k + 1) % hull.size();
-          support_k.point = hull.get(index_k);
-        }else if(index_i == index_l){
-          index_l = (index_l + 1) % hull.size();
-          support_l.point = hull.get(index_l);
-        }
-				 */
+				// Le point i a bougé (condition d'arrêt)
 				iStepped = true;
 			} else if (index_cos_max == index_j) {
 				support_j = new Line(hull.get(index_j), hull.get((index_j + 1)
 						% hull.size()));
-				support_k = new Line(hull.get(index_k),
-						support_j.director.normal().invert());
+				support_k = new Line(hull.get(index_k), support_j.director
+						.normal().invert());
 				support_l = new Line(hull.get(index_l),
 						support_j.director.invert());
-				support_i = new Line(hull.get(index_i), support_k.director
-						.invert());
+				support_i = new Line(hull.get(index_i),
+						support_k.director.invert());
 
 				index_j = (index_j + 1) % hull.size();
 				support_j.point = hull.get(index_j);
-
-				/*if(index_j == index_k){
-          index_k = (index_k + 1) % hull.size();
-          support_k.point = hull.get(index_k);
-        }else if(index_j == index_l){
-          index_l = (index_l + 1) % hull.size();
-          support_l.point = hull.get(index_l);
-        } 
-        else if(index_j == index_i){
-          index_i = (index_i + 1) % hull.size();
-          support_i.point = hull.get(index_i);
-        }
-				 */
+				// Le point j a bougé (condition d'arrêt)
 				jStepped = true;
 
 			} else if (index_cos_max == index_k) {
 				support_k = new Line(hull.get(index_k), hull.get((index_k + 1)
 						% hull.size()));
-				support_l = new Line(hull.get(index_l),
-						support_k.director.normal().invert());
+				support_l = new Line(hull.get(index_l), support_k.director
+						.normal().invert());
 				support_i = new Line(hull.get(index_i),
 						support_k.director.invert());
-				support_j = new Line(hull.get(index_j), support_l.director
-						.invert());
+				support_j = new Line(hull.get(index_j),
+						support_l.director.invert());
 				index_k = (index_k + 1) % hull.size();
 				support_k.point = hull.get(index_k);
-				/*
-        if(index_k == index_j){
-          index_j = (index_j + 1) % hull.size();
-          support_j.point = hull.get(index_j);
-        }else if(index_k == index_i){
-          index_i = (index_i + 1) % hull.size();
-          support_i.point = hull.get(index_i);
-        }else if(index_k == index_l){
-          index_l = (index_l + 1) % hull.size();
-          support_l.point = hull.get(index_l);
-        }
-				 */
+				// Le piont k a bougé (condition d'arrêt)
 				kStepped = true;
 
 			} else {
 
 				support_l = new Line(hull.get(index_l), hull.get((index_l + 1)
 						% hull.size()));
-				support_i = new Line(hull.get(index_i),
-						support_l.director.normal().invert());
+				support_i = new Line(hull.get(index_i), support_l.director
+						.normal().invert());
 				support_j = new Line(hull.get(index_j),
 						support_l.director.invert());
-				support_k = new Line(hull.get(index_k), support_i.director
-						.invert());
+				support_k = new Line(hull.get(index_k),
+						support_i.director.invert());
 				index_l = (index_l + 1) % hull.size();
 				support_l.point = hull.get(index_l);
-				/*
-        if(index_l == index_j){
-          index_j = (index_j + 1) % hull.size();
-          support_j.point = hull.get(index_j);
-        }else if(index_l == index_k){
-          index_k = (index_k + 1) % hull.size();
-          support_k.point = hull.get(index_k);
-        }else if(index_l == index_i){
-          index_i = (index_i + 1) % hull.size();
-          support_i.point = hull.get(index_i);
-        }
-				 */
+				// Le point l a bougé (condition d'arrêt)
 				lStepped = true;
 
 			}
 
-			/*System.out.println("cosI " + cos_theta_i);
-			System.out.println("cosJ " + cos_theta_j);
-			System.out.println("cosK " + cos_theta_k);
-			System.out.println("cosL " + cos_theta_l);
-			System.out.println();
-			 */
-			// 5 : Get rectangle's area.
-			rect = new Rectangle(support_i, support_j, support_k,
-					support_l);
+			// Cinquième étape : On calcule l'aire du rectangle.
 
+			rect = new Rectangle(support_i, support_j, support_k, support_l);
+			// Mise à jour du rectangle minimum
 			if (rect.area() < areaMin) {
 				res = rect;
 				areaMin = rect.area();
 			}
-
+			// Si l'un des points a bougé et qu'il est revenu à son point de
+			// départ
 			if (iStepped && index_i == index_i0 || jStepped
 					&& index_j == index_j0 || kStepped && index_k == index_k0
 					|| lStepped && index_l == index_l0) {
+				// On incrémente le compteur.
 				count++;
 			}
+			// Condition d'arrêt
 			hullScanned = (count >= 4);
 		}
+		// On renvoie le plus petit rectangle
 		return res;
 	}
 
